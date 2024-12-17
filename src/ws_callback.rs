359b7dee2255b483af6da663550985bb66b34f4e
amigin <amigin@gmail.com>
2024-12-17 17:00:28 +0200

    Naming Fix

--- src/ws_callback.rs
+++ src/ws_callback.rs
@@ -6,14 +6,17 @@ use url_utils::UrlBuilder;
 use super::WsConnection;
 
 #[derive(Default)]
-pub struct StartConnectionResult {
+pub struct StartWsConnectionDataToApply {
     pub headers: Option<Vec<(String, String)>>,
     pub url: Option<UrlBuilder>,
 }
 
 #[async_trait::async_trait]
 pub trait WsCallback {
-    async fn before_start_ws_connect(&self, url: String) -> Result<StartConnectionResult, String>;
+    async fn before_start_ws_connect(
+        &self,
+        url: String,
+    ) -> Result<StartWsConnectionDataToApply, String>;
     async fn on_connected(&self, ws_connection: Arc<WsConnection>);
     async fn on_disconnected(&self, ws_connection: Arc<WsConnection>);
     async fn on_data(&self, ws_connection: Arc<WsConnection>, data: Message);
